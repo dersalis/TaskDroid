720c32160ce69c9a0874a7cecb06c62e420413f6
Damian <hello@damianruta.pl>
2023-05-29 13:50:01 +0200

    TaskByPriority

--- TaskDroid.Mobile/src/app/pages/tasks-by-priority/tasks-by-priority.page.ts
+++ TaskDroid.Mobile/src/app/pages/tasks-by-priority/tasks-by-priority.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TaskPriority } from 'src/app/shared/enums/taskPriority';
 
 @Component({
   selector: 'app-tasks-by-priority',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksByPriorityPage implements OnInit {
 
-  constructor() { }
+  private priority: TaskPriority | null = null;
+
+  constructor(private activateRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activateRoute.paramMap.subscribe(paramMap => {
+      if(!paramMap.has('priorityId')) {
+        return;
+      }
+      const priorityId: number = Number(paramMap.get('priorityId'));
+      console.log('priorityId: ', priorityId);
+      this.priority = priorityId as TaskPriority;
+      console.log('priority: ', this.priority);
+    });
   }
 
 }
